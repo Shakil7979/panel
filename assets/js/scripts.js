71f59a0188646b5a0482ba05a5fafa9acccedffd
Sadbin Shakil <71561507+Shakil7979@users.noreply.github.com>
2023-12-05 02:43:59 +0600

    update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,22 +1,51 @@
-$(document).ready(function(){
-	// Mobile Menu
-	// $('.logo a i').click(function(){
-	// 	$('.menu ul').slideToggle(1000);
-
-	// 	return false
-	// });
-
-	// Banner Carousel
-	// $('.banner_carousel').owlCarousel({
-	// 	items:1,
-	// 	loop:true,
-	// 	nav:true,
-	// 	dots:true,
-	// 	autoplay:true,
-	// });
-
-
-
-
-
+ 
+ 
+function format(d) { 
+    return (
+        '<dl>' +
+        '<dt>Full name:</dt>' +
+        '<dd>' +
+        d.name +
+        '</dd>' +
+        '<dt>Extension number:</dt>' +
+        '<dd>' +
+        d.extn +
+        '</dd>' +
+        '<dt>Extra info:</dt>' +
+        '<dd>And any further details here (images etc)...</dd>' +
+        '</dl>'
+    );
+}
+ 
+let table = new DataTable('#example', {
+    ajax: 'json_data.json',
+    columns: [
+        {
+            className: 'dt-control',
+            orderable: false,
+            data: null,
+            defaultContent: ''
+        },
+        { data: 'Produkt' },
+        { data: 'Status' },
+        { data: 'Sprzedanych' },
+        { data: 'Przychód' }
+    ],
+    order: [[1, 'asc']]
+});
+ 
+// Add event listener for opening and closing details
+table.on('click', 'td.dt-control', function (e) {
+    let tr = e.target.closest('tr');
+    let row = table.row(tr);
+ 
+    if (row.child.isShown()) {
+        // This row is already open - close it
+        row.child.hide();
+    }
+    else {
+        // Open this row
+        row.child(format(row.data())).show();
+    }
+	
 });
