0482dd8d133780d2223592dfab92c0a40405d31c
Sadbin Shakil <71561507+Shakil7979@users.noreply.github.com>
2023-12-07 16:49:51 +0600

    update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -20,9 +20,16 @@ $(document).ready(function(){
     });
 
 
-	$(document).on('click','.mobile_bars a', function(){
+	$(document).on('click','.menu_icon', function(){
 		$('.desktop_menu').slideToggle();
 	});
+
+	$(document).ready(function(){
+		$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
+			$(this).toggleClass('open');
+		});
+	});
+	
 
 
 
